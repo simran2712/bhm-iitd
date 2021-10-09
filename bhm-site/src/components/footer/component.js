cce4f5248dc29d7b46e21c66e4e098fdfdfa10e2
Jasraj Singh Bhatia <singhbhatiajasraj@gmail.com>
2021-10-09 20:20:03 +0530

    Minor UI Changes
    Made texts smaller, responsive

--- bhm-site/src/components/footer/component.js
+++ bhm-site/src/components/footer/component.js
@@ -1,12 +1,13 @@
 import './component.css';
 
 function Footer() {
-    return <div className="footer">
+    return <div className="footer py-2">
         <div className="container mt-4">
-            <h4 className="font-weight-bold text-center">Hostels</h4>
             <div class="row ">
-                <div className="col-6 footer_hostels">
+                <div className="col-6 col-md-2 footer_hostels">
                     <ul>
+                        <span className="font-weight-bold footer-heading">Hostels</span><br />
+
                         <li><a href="">Aravali</a></li>
                         <li><a href="">Girnar</a></li>
                         <li><a href="">Himadri</a></li>
@@ -16,8 +17,10 @@ function Footer() {
                         <li><a href="">Kumaon</a></li>
                     </ul>
                 </div>
-                <div className="col-6 footer_hostels">
+                <div className="col-6 col-md-3 footer_hostels">
                     <ul>
+                        <span className="font-weight-bold">&nbsp;</span>
+                        <span className="font-weight-bold">&nbsp;</span>
                         <li><a href="">Nilgiri</a></li>
                         <li><a href="">Satpura</a></li>
                         <li><a href="">Shivalik</a></li>
@@ -29,21 +32,22 @@ function Footer() {
             </div>
             {/* </div>
         <div className="container"> */}
-            <div className="">
+            {/* <div className="">
                 <h3 className="font-weight-bold text-center">Contact Us</h3>
                 <hr />
-            </div>
-            <div className="row">
-                <div className="">
-                    <h4 className="text-left mb-2 font-weight-bold">BHM IIT Delhi</h4>
-                    <ul className="nav">
-                        <li className="nav-item contact-li"><a href="https://www.linkedin.com/company/board-for-hostel-management" className="nav-link"><i className="fa fa-linkedin fa-lg contact-icon"></i></a></li>
-                        <li className="nav-item contact-li"><a href="https://www.instagram.com/bhmiitd/" className="nav-link"><i className="fa fa-instagram fa-lg contact-icon"></i></a></li>
-                        <li className="nav-item contact-li"><a href="https://m.facebook.com/bhm.iitd" className="nav-link"><i className="fa fa-facebook fa-lg contact-icon"></i></a></li>
-                    </ul>
-                    <br />
-                </div>
-                {/* <div className="col-md-6">
+            </div> */}
+            <div className="container">
+                <div className="row">
+                    <div className="">
+                        <h6 className="text-left mb-2 font-weight-bold">BHM IIT Delhi</h6>
+                        <ul className="nav">
+                            <li className="nav-item contact-li"><a href="https://www.linkedin.com/company/board-for-hostel-management" className="footer-link"><i className="fa fa-linkedin contact-icon"></i></a></li>
+                            <li className="nav-item contact-li"><a href="https://www.instagram.com/bhmiitd/" className="footer-link"><i className="fa fa-instagram contact-icon"></i></a></li>
+                            <li className="nav-item contact-li"><a href="https://m.facebook.com/bhm.iitd" className="-link"><i className="fa fa-facebook  contact-icon"></i></a></li>
+                        </ul>
+                        <br />
+                    </div>
+                    {/* <div className="col-md-6">
                     <form>
                         <fieldset className="form-group">
                             <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
@@ -56,12 +60,18 @@ function Footer() {
                         </fieldset>
                     </form>
                 </div> */}
+                </div>
             </div>
+            <hr className="m-0" />
+            <div className="footer-text pt-2 text-left ">
+                <span>Copyright &copy; Board for Hostel Management - IIT Delhi. All rights reserved.</span>
+            </div>
+
         </div>
-        <div className="enquire mr-5">
+        {/* <div className="enquire mr-5">
             <a href="#" >Enquire Now</a>
-        </div>
-        <h5 style={{ float: "left" }} className="ml-5">Copyright &#169; 2021 BHM IIT Delhi</h5>
+        </div> */}
+        {/* <h5 style={{ float: "left" }} className="ml-5">Copyright &#169; 2021 BHM IIT Delhi</h5> */}
     </div>
 }
 
