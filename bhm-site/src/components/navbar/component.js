5f1b9514aeaca8055f2f599139e747f9f90a3999
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2021-11-05 15:26:45 +0530

    Update component.js

--- bhm-site/src/components/navbar/component.js
+++ bhm-site/src/components/navbar/component.js
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom";
 function Navbar(){
     return <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
-    <Link class="navbar-brand" to="/bhm-site">BHM - IIT Delhi</Link>
+    <Link class="navbar-brand" to="/">BHM - IIT Delhi</Link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav navbar-nav-scroll mx-auto">
       <li class="nav-item">
-        <Link class="nav-link" to="/bhm-site">Home</Link>
+        <Link class="nav-link" to="/">Home</Link>
         </li>
         <li class="nav-item">
         <Link class="nav-link" to="/about-us">About Us</Link>
