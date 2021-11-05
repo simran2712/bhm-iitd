94c9c5f98b1b7338dde758dfc7c6eaa4cd816ee1
Jasraj Singh Bhatia <singhbhatiajasraj@gmail.com>
2021-11-05 19:29:52 +0530

    included build folder

---
+++ bhm-site/build/index.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 10) {
+        $('#navBar').addClass('floatingNav');
+    } else {
+        $('#navBar').removeClass('floatingNav');
+    }
+});
+
+$( document ).ready(function() {
+    $('#myModal').modal('show')
+});
