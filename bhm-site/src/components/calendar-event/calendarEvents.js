c624fa5592d7a7b2eaea72646ba9c6c80df0843f
Anurag Gupta <75490384+guptaanurag2106@users.noreply.github.com>
2021-11-06 10:36:40 +0530

    add Template json for calendar-events

---
+++ bhm-site/src/components/calendar-event/calendarEvents.js
@@ -0,0 +1,81 @@
+// date: {
+//     type: Date,
+//     required: true
+// },
+// heading:{
+//     type:String,
+//     required:true,
+// },
+// notice: {
+//     type: String,
+//     required: true,
+// },
+// notifType: {
+//     type: ['popup', 'normal'],
+//     required: false,
+//     default: 'normal'
+// },
+// show: {
+//     type: Boolean,
+//     required: true,
+//     default: true
+// }
+
+export const notifications = [
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "normal",
+        show: true
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "popup",
+        show: true
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "normal",
+        show: false
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "normal",
+        show: true
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "popup",
+        show: false
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "normal",
+        show: false
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "popup",
+        show: true
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        notifType: "normal",
+        show: false
+    },
+]
