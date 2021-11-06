e97cc423be838857fecc4e2f70872fc28901e169
Anurag Gupta <75490384+guptaanurag2106@users.noreply.github.com>
2021-11-06 10:37:46 +0530

    add template json for notice-board

---
+++ bhm-site/src/components/notices/noticeBoard.js
@@ -0,0 +1,59 @@
+// date: {
+//     type: Date,
+//     required: true
+// },
+// heading:{
+//     type:String,
+//     required:true,
+// },
+// notice: {
+//     type: String,
+//     required: true,
+// },
+// hostel: {
+//     type: ['aravali', 'girnar', 'himadri', 'jwalamukhi', 'kailash', 'karakoram', 'kumaon', 'nilgiri', 'satpura', 'shivalik', 'vindhyachal, 'zanskar'],
+//     required: true,
+// },
+// show: {
+//     type: Boolean,
+//     required: true,
+//     default: true,
+// }
+
+export const notifications = [
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        hostel: "aravali",
+        show: true
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        hostel: "aravali",
+        show: true
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        hostel: "vindhyachal",
+        show: false
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        hostel: "zanskar",
+        show: true
+    },
+    {
+    	date: new Date(),
+    	heading: "Lorem ipsum dolor",
+        notice: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat consequat maximus. In convallis vel nisi at dapibus. Ut vulputate turpis eu lectus euismod pretium. Phasellus eget dignissim ipsum. Nulla vitae hendrerit ante, quis volutpat augue. Duis posuere eget nisl fringilla venenatis. Nulla fringilla dolor sit amet hendrerit maximus. Sed luctus, tellus eget rhoncus rutrum, mi lacus vestibulum sapien, eget faucibus urna justo vel nunc.",
+        hostel: "satpura",
+        show: false
+    },
+]
