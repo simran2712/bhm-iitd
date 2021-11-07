6be6c54535ff644cbd4dd316c4e2ebc6581bfe74
Jasraj Singh Bhatia <singhbhatiajasraj@gmail.com>
2021-11-07 21:24:59 +0530

    Hostel Components
    Added Alumni and Student Team

--- bhm-site/src/components/hostel/component.js
+++ bhm-site/src/components/hostel/component.js
@@ -3,7 +3,8 @@ import {useParams} from 'react-router-dom';
 import Introduction from './hostel-page-introduction/component';
 import Vision from './hostel-page-vision/component';
 import ContactUs from './hostel-page-contact-us/component';
-
+import Alumni from './hostel-alumni/component'
+import StudentTeam from './hostel-student-team/component';
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -18,6 +19,9 @@ function Hostel(props) {
     <Introduction/>
     <Vision/>
     <ContactUs/>
+    <Alumni/>
+    <StudentTeam/>
+
   </div>
 
 }
