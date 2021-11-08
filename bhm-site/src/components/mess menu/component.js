2475ed59ba38f891bd91a17cabfa77dda58f0caf
simran2712 <83649558+simran2712@users.noreply.github.com>
2021-11-08 13:21:44 +0530

    Mess menu

--- bhm-site/src/components/mess menu/component.js
+++ bhm-site/src/components/mess menu/component.js
@@ -1,22 +1,47 @@
 import './component.css'
-import './script'
-import { ReactDOM } from 'react';
-import ScriptTag from 'react-script-tag';
 import { Link } from 'react-router-dom';
+
 function Messmenu(){
-    const script=document.createElement("script"); script.async=true; script.src="./script";
-    return <div>
+    var marker=document.querySelector('#marker');
+var item=document.querySelectorAll('.menu a');
+function indicator(e){
+    marker.style.left=e.offsetLeft+"px";
+    marker.style.width=e.offsetWidth+"px";
+    e.style.color="#000";
+}
+item.forEach(link => {
+    link.addEventListener('click',(e)=>{
+        indicator(e.target);
+    })
+});
+    return <div className="container-fluid">
+        <div className="heading">Mess Menu</div>
         <div class="menu">
     <div id="marker"></div>
+    <Link to="#">Sunday</Link>
     <Link to="#">Monday</Link>
-    <Link to="#">Monday</Link>
-    <Link to="#">Monday</Link>
-    <Link to="#">Monday</Link>
-    <Link to="#">Monday</Link>
-    <Link to="#">Monday</Link>
-    <Link to="#">Monday</Link>
+    <Link to="#">Tuesday</Link>
+    <Link to="#">Wednesday</Link>
+    <Link to="#">Thursday</Link>
+    <Link to="#">Friday</Link>
+    <Link to="#">Saturday</Link>
 </div>
+<div id="menu-container" className="container">
+        <div class="box">
+            <h3>Lunch</h3>
+            <p>This is the lunch menu for respective hostel</p>
+        </div>
+        <div class="box">
+            <h3>Lunch</h3>
+            <p>This is the lunch menu for respective hostel</p>
+        </div>
+        <div class="box">
+            <h3>Lunch</h3>
+            <p>This is the lunch menu for respective hostel</p>
+        </div>
+    </div>
 </div>
+
 }
 
 export default Messmenu;
