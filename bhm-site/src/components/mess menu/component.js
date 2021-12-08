9b78bae29f02c0460ccb6f7ec82bd1a836e61362
simran2712 <83649558+simran2712@users.noreply.github.com>
2021-12-09 04:31:24 +0530

    Mess menu changes

--- bhm-site/src/components/mess menu/component.js
+++ bhm-site/src/components/mess menu/component.js
@@ -1,43 +1,38 @@
 import './component.css'
 import { Link } from 'react-router-dom';
-var marker=document.querySelector('#marker');
-var item=document.querySelectorAll('.menu a');
-function indicator(e){
-    marker.style.left=e.offsetLeft+"px";
-    marker.style.width=e.offsetWidth+"px";
-    e.style.color="#000";
-}
-item.forEach(link => {
-    link.addEventListener('click',(e)=>{
-        indicator(e.target);
-    })
-});
+
 function Messmenu(){
     
     return <div id="mess-menu" className="container-fluid">
         <div className="heading" id="mess-heading">Mess Menu</div>
         <div class="menu">
     <div id="marker"></div>
-    <Link to="#">Sunday</Link>
-    <Link to="#">Monday</Link>
-    <Link to="#">Tuesday</Link>
-    <Link to="#">Wednesday</Link>
-    <Link to="#">Thursday</Link>
-    <Link to="#">Friday</Link>
-    <Link to="#">Saturday</Link>
+    <Link class="link1" to="/">Sunday</Link>
+    <Link class="link1" to="/">Monday</Link>
+    <Link class="link1" to="/">Tuesday</Link>
+    <Link class="link1" to="/">Wednesday</Link>
+    <Link class="link1" to="/">Thursday</Link>
+    <Link class="link1" to="/">Friday</Link>
+    <Link class="link1" to="/">Saturday</Link>
 </div>
 <div id="menu-container" className="container">
         <div class="box">
-            <h3>Lunch</h3>
-            <p>This is the lunch menu for respective hostel</p>
+            {/* <h3>Lunch</h3> */}
+            <i class="fas fa-hamburger pr-2">Lunch</i>
+            <h6>12:30PM-2:30PM</h6>
+            <p>This is the lunch menu for the respective hostel</p>
         </div>
         <div class="box">
-            <h3>Lunch</h3>
-            <p>This is the lunch menu for respective hostel</p>
+            {/* <h3>Lunch</h3> */}
+            <i class="fas fa-hamburger pr-2">Lunch</i>
+            <h6>12:30PM-2:30PM</h6>
+            <p>This is the lunch menu for the respective hostel</p>
         </div>
         <div class="box">
-            <h3>Lunch</h3>
-            <p>This is the lunch menu for respective hostel</p>
+            {/* <h3>Lunch</h3> */}
+            <i class="fas fa-hamburger pr-2">Lunch</i>
+            <h6>12:30PM-2:30PM</h6>
+            <p>This is the lunch menu for the respective hostel</p>
         </div>
     </div>
 </div>
